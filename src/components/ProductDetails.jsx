61528fe732227274a6031cda4edc5653a302c7f1
jeremygigase <32205202+jeremygigase@users.noreply.github.com>
2022-09-12 16:15:24 +0100

    finished workshop

---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,3 @@
+export default function ProductDetails({product}) {
+  return <p>Hello World! This is the product page for {product.title}</p>;
+ }
---
+++ src/routes/products/[handle].server.jsx
@@ -0,0 +1,66 @@
+import { useShopQuery, gql } from '@shopify/hydrogen';
+import Layout from '../../components/Layout.server';
+import ProductDetails from '../../components/ProductDetails.client';
+
+export default function Example({ params }) {
+  const { handle } = params;
+  const { data } = useShopQuery({
+    query: QUERY,
+    variables: {
+      handle,
+    },
+  });
+
+  console.log(data);
+
+  return (
+    <Layout>
+      <ProductDetails product={data.product} />
+    </Layout>
+  );
+}
+
+const QUERY = gql`
+  query product($handle: String!) {
+    product(handle: $handle) {
+      id
+      title
+      handle
+      vendor
+      descriptionHtml
+      media(first: 10) {
+        nodes {
+          ... on MediaImage {
+            mediaContentType
+            image {
+              id
+              url
+              altText
+              width
+              height
+            }
+          }
+        }
+      }
+      variants(first: 250) {
+        nodes {
+          id
+          title
+          availableForSale
+          priceV2 {
+            currencyCode
+            amount
+          }
+          compareAtPriceV2 {
+            currencyCode
+            amount
+          }
+          selectedOptions {
+            name
+            value
+          }
+        }
+      }
+    }
+  }
+`;
